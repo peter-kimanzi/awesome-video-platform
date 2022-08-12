f05347202f7ae172e539245f2c4730a8effa49e4
peter kimanzi <kimjayden001@gmail.com>
2022-08-12 11:43:24 +0300

    everything

---
+++ script.js
@@ -0,0 +1,55 @@
+$(function () {
+ $(".sidebar-link").click(function () {
+  $(".sidebar-link").removeClass("is-active");
+  $(this).addClass("is-active");
+ });
+});
+
+$(window)
+ .resize(function () {
+  if ($(window).width() > 1090) {
+   $(".sidebar").removeClass("collapse");
+  } else {
+   $(".sidebar").addClass("collapse");
+  }
+ })
+ .resize();
+
+const allVideos = document.querySelectorAll(".video");
+
+allVideos.forEach((v) => {
+ v.addEventListener("mouseover", () => {
+  const video = v.querySelector("video");
+  video.play();
+ });
+ v.addEventListener("mouseleave", () => {
+  const video = v.querySelector("video");
+  video.pause();
+ });
+});
+
+$(function () {
+ $(".logo, .logo-expand, .discover").on("click", function (e) {
+  $(".main-container").removeClass("show");
+  $(".main-container").scrollTop(0);
+ });
+ $(".trending, .video").on("click", function (e) {
+  $(".main-container").addClass("show");
+  $(".main-container").scrollTop(0);
+  $(".sidebar-link").removeClass("is-active");
+  $(".trending").addClass("is-active");
+ });
+
+ $(".video").click(function () {
+  var source = $(this).find("source").attr("src");
+  var title = $(this).find(".video-name").text();
+  var person = $(this).find(".video-by").text();
+  var img = $(this).find(".author-img").attr("src");
+  $(".video-stream video").stop();
+  $(".video-stream source").attr("src", source);
+  $(".video-stream video").load();
+  $(".video-p-title").text(title);
+  $(".video-p-name").text(person);
+  $(".video-detail .author-img").attr("src", img);
+ });
+});
